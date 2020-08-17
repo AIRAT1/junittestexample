f38bcdc7cbea8c372ffda2cb70d52e46393ce634
Ayrat <ayrat1@mail.ru>
2020-08-17 13:05:52 +0300

    add more tests

--- src/main/java/ee/era/hangman/model/Hangman.java
+++ src/main/java/ee/era/hangman/model/Hangman.java
@@ -1,13 +1,44 @@
 package ee.era.hangman.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Hangman {
     private final String word;
+    private String maskedWord;
+    private int failures;
+    private Set<Character> usedLetters = new HashSet<>();
 
     public Hangman(String word) {
         this.word = word;
+        maskedWord = word.replaceAll(".", "_");
     }
 
     public String getWord() {
-        return "____";
+        return maskedWord;
+    }
+
+    public void guessLetter(char letter) {
+        int index = word.indexOf(letter);
+        if (index < 0) {
+            if (!usedLetters.contains(letter)) {
+                failures++;
+                usedLetters.add(letter);
+            }
+            return;
+        }
+        maskedWord = maskedWord.substring(0, index) + letter + maskedWord.substring(index + 1);
+    }
+
+    public boolean isLost() {
+        return failures > 5;
+    }
+
+    public boolean isWon() {
+        return !maskedWord.contains("_");
+    }
+
+    public int getFailuresCount() {
+        return failures;
     }
 }
--- src/test/java/ee/era/hangman/model/HangmanTest.java
+++ src/test/java/ee/era/hangman/model/HangmanTest.java
@@ -2,11 +2,78 @@ package ee.era.hangman.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class HangmanTest {
+    Hangman game = new Hangman("sofa");
+
     @Test
     public void youSeeMaskedWord() {
-        assertEquals("____", new Hangman("sofa").getWord());
+        assertEquals("____", game.getWord());
+    }
+
+    @Test
+    public void gameIsNotLostAtTheBeginning() {
+        assertFalse(game.isLost());
+        assertEquals(0, game.getFailuresCount());
+    }
+
+    @Test
+    public void youMustGuessLetters() {
+        game.guessLetter('o');
+        assertEquals("_o__", game.getWord());
+        assertEquals(0, game.getFailuresCount());
+    }
+
+    @Test
+    public void youHaveSixGuesses() {
+        game.guessLetter('x');
+        game.guessLetter('y');
+        game.guessLetter('z');
+        game.guessLetter('u');
+        game.guessLetter('t');
+        assertFalse(game.isLost());
+        assertEquals(5, game.getFailuresCount());
+        game.guessLetter('i');
+        assertTrue(game.isLost());
+        assertEquals(6, game.getFailuresCount());
+    }
+
+    @Test
+    public void youWinWhenYouGuessedAllLetters() {
+        game.guessLetter('f');
+        game.guessLetter('s');
+        game.guessLetter('a');
+        game.guessLetter('o');
+        assertTrue(game.isWon());
+        assertEquals(0, game.getFailuresCount());
+    }
+
+    @Test
+    public void multipleClicks() {
+        game.guessLetter('q');
+        game.guessLetter('q');
+        game.guessLetter('q');
+        game.guessLetter('q');
+        game.guessLetter('q');
+        game.guessLetter('q');
+        game.guessLetter('q');
+        assertEquals(1, game.getFailuresCount());
+        assertFalse(game.isWon());
+        assertFalse(game.isLost());
+    }
+
+    @Test
+    public void multipleCorrectClicks() {
+        game.guessLetter('s');
+        game.guessLetter('s');
+        game.guessLetter('s');
+        game.guessLetter('s');
+        game.guessLetter('s');
+        game.guessLetter('s');
+        game.guessLetter('s');
+        assertEquals(0, game.getFailuresCount());
+        assertFalse(game.isWon());
+        assertFalse(game.isLost());
     }
 }
